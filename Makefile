db8cd7d3cad21e67cc6d1ccb5565a891382becfe
makiri1993 <kireew.martin@icloud.com>
2020-10-05 19:30:23 +0200

    add env variable for db

--- Makefile
+++ Makefile
@@ -2,3 +2,5 @@ build:
 	docker build -t st0cky-bot .
 run:
 	docker run -it --rm --name st0cky-bot st0cky-bot
+local-run:
+	db="host=127.0.0.1 port=5432 user=postgres dbname=postgres password=password sslmode=disable" go run main.go
--- setup/setup.go
+++ setup/setup.go
@@ -6,6 +6,7 @@ import (
 	"github.com/jinzhu/gorm"
 	. "gopkg.in/tucnak/telebot.v2"
 	"log"
+	"os"
 	"time"
 )
 
@@ -13,7 +14,6 @@ const (
 	Endpoint = "https://search-news.cognitiveservices.azure.com/bing/v7.0/news/search"
 	Token    = "17dfa447c0734e158d858e6f45526ff3"
 	botToken = "1245803898:AAFZtTRZYdCk5rdCo27hHc9Wrlbmr5E46DY"
-	db       = "host=127.0.0.1 port=5432 user=postgres dbname=postgres password=password sslmode=disable"
 )
 
 var DbConn *gorm.DB
@@ -39,7 +39,7 @@ func InitBot() {
 
 func InitDb() {
 	var err error
-	DbConn, err = gorm.Open("postgres", db)
+	DbConn, err = gorm.Open("postgres", os.Getenv("db"))
 	if err != nil {
 		panic(fmt.Sprintf("failed to connect to database Error:%s", err))
 	}
