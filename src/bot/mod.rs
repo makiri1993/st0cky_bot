76b0d7360b37f3c9571f9266fde1728a99e8ff0b
makiri1993 <kireew.martin@icloud.com>
2021-06-02 08:32:02 +0200

    Some refactoring + remove bing key

--- src/bot/mod.rs
+++ src/bot/mod.rs
@@ -2,6 +2,7 @@ use crate::models::api::BingNewsApiResponse;
 use crate::models::keyword::handle_keyword_creation;
 use crate::models::user::handle_user_creation;
 use sqlx::{Pool, Postgres};
+use std::env;
 use std::error::Error;
 use teloxide::payloads::SendMessageSetters;
 use teloxide::{prelude::*, utils::command::BotCommand};
@@ -12,6 +13,7 @@ pub async fn run(connection: Pool<Postgres>) {
 
     let bot = Bot::from_env().auto_send();
     let bot_name: String = "st0cky".to_string();
+    let client = reqwest::Client::new();
 
     type DispatcherHandler<T> = DispatcherHandlerRx<AutoSend<Bot>, T>;
 
@@ -21,9 +23,11 @@ pub async fn run(connection: Pool<Postgres>) {
                 .commands(bot_name)
                 .for_each_concurrent(None, move |(context, command)| {
                     let connection_clone = connection.clone();
-
+                    let client_clone = client.clone();
                     async move {
-                        answer(context, command, connection_clone).await.unwrap();
+                        answer(context, command, connection_clone, client_clone)
+                            .await
+                            .unwrap();
                     }
                 })
         })
@@ -48,10 +52,14 @@ fn get_telegram_details(cx: &UpdateWithCx<AutoSend<Bot>, Message>) -> (i64, Opti
     (x.to_owned().id, x.to_owned().username)
 }
 
+const BING_URL: &str = "https://api.cognitive.microsoft.com/bing/v7.0/news/search";
+const BING_HEADER_KEY: &str = "Ocp-Apim-Subscription-Key";
+
 async fn answer(
     cx: UpdateWithCx<AutoSend<Bot>, Message>,
     command: Command,
     connection: Pool<Postgres>,
+    client: reqwest::Client,
 ) -> Result<(), Box<dyn Error + Send + Sync>> {
     let telegram_details = get_telegram_details(&cx);
     log::info!("Received a command from user {:?}", telegram_details.0);
@@ -68,14 +76,11 @@ async fn answer(
             .await?
         }
         Command::GetNews(searchterm) => {
-            let client = reqwest::Client::new();
-            let request_url = "https://api.cognitive.microsoft.com/bing/v7.0/news/search";
-
             let response = client
-                .get(request_url)
+                .get(BING_URL)
                 .header(
-                    "Ocp-Apim-Subscription-Key",
-                    "17dfa447c0734e158d858e6f45526ff3",
+                    BING_HEADER_KEY,
+                    env::var("BING_API_TOKEN").expect("BING_API_TOKEN not set"),
                 )
                 .query(&[("q", &searchterm)])
                 .send()
@@ -83,14 +88,13 @@ async fn answer(
                 .json::<BingNewsApiResponse>()
                 .await?;
 
-            let mut test = "".to_string();
-            response.value.iter().for_each(|news| {
-                test = format!(
+            let pretty_printed_news = response.value.iter().fold("".to_string(), |curr, next| {
+                format!(
                     "{}<b><u>🔔 {}</u></b>\n\n{}\n\n🤓 <a href=\"{}\">Link</a>\n\n\n\n",
-                    test, news.name, news.description, news.url
-                );
+                    curr, next.name, next.description, next.url
+                )
             });
-            cx.answer(test)
+            cx.answer(pretty_printed_news)
                 .parse_mode(teloxide::types::ParseMode::Html)
                 .await?
         }
